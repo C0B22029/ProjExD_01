4c7aa3d59067a5fbd74cb4a9ba72fc418569dc33
rayohno <c0b2202940@edu.teu.ac.jp>
2023-09-26 16:45:22 +0900

    演習１

--- pygame_image.py
+++ pygame_image.py
@@ -7,13 +7,15 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False)
     tmr = 0
     
     
     tori_img = pg.image.load("ex01/fig/3.png")
     tori_img = pg.transform.flip(tori_img,True,False)
     tori_img2 = pg.transform.rotozoom(tori_img,-10,1.0)
-    tori_imgs = [tori_img,tori_img2]
+    tori_imgs = [tori_img,tori_img,tori_img,tori_img,tori_img,tori_img,tori_img,tori_img,tori_img,tori_img,
+                 tori_img2,tori_img2,tori_img2,tori_img2,tori_img2,tori_img2,tori_img2,tori_img2,tori_img2,tori_img2]
     
     
     
@@ -24,12 +26,13 @@ def main():
         x = tmr%1600
         
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x, 0])
-        screen.blit(tori_imgs[tmr%2], [300,200])
+        screen.blit(bg_img2, [1600-x, 0])
+        # screen.blit(bg_img, [3200-x, 0])
+        screen.blit(tori_imgs[tmr%20], [300,200])
         
         pg.display.update()
         tmr += 1        
-        clock.tick(100)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
